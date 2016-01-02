20d7b1b466567497f9ab0fe6a0f57a2048231ddc
jerkos <cram@hotmail.fr>
2016-01-02 22:37:36 +0100

    add lmsd.sqlite

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='mzOS',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
+    include_package_data=True,
     url='http://github.com/jerkos/mzOS',
     license='MIT',
     author='Marco',
     author_email='cram@hotmail.fr',
     description='Heuristic based feature annotations/identifications of LC-MS metabolomics dataset.',
     long_description=open('README.md').read(),
-    requires=['bioservices', 'scipy', 'numpy', 'scikit-learn', 'six'],
+    requires=['bioservices', 'scipy', 'numpy', 'six', 'sklearn'],
     classifiers=['Development Status :: 3 - Alpha',
                  'License :: OSI Approved :: MIT License',
                  'Programming Language :: Python :: 2.7',
